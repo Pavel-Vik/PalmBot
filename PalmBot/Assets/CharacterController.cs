ba80994db1954d62e105c565b9a29edb836c0d61
Pavel-Vik <pavel.ale.vik@gmail.com>
2019-07-04 17:38:24 +0200

    Demo 1.1 (movement)
    Added normal smooth movement, colliders, green trigger zone.

---
+++ PalmBot/Assets/CharacterController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{       // Game objects
+    public GameObject gameController;
+    public GameObject tree;
+
+        // Movement vars
+    public float movementSpeed = 1f;
+    public float xStep = -0.5f;
+    public float yStep = 0.25f;
+    public Vector2 target;
+
+    Rigidbody2D rbody;
+    public bool isReadyToPlant = false;
+
+    private Vector2 finishCharacterPos;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        target = transform.position;
+        finishCharacterPos = new Vector2(2, 4);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Move();
+
+        transform.position = Vector3.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
+    }
+
+    public void Move()
+    {
+        target = new Vector2(target.x + xStep, target.y + yStep);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Green")
+        {
+            Debug.Log("Green Zone entered");
+            if (gameController.GetComponent<GameController>().plantTreeCommanded == true)
+                Instantiate(tree);
+        }
+        if (collision.tag == "Collider")
+        {
+            Debug.Log("Collider entered");
+            target = new Vector2(target.x - xStep, target.y - yStep);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isReadyToPlant = false;
+    }
+}
---
+++ PalmBot/Assets/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameController : MonoBehaviour
+{
+    CommandPanel commandsPanel; // Our current panel
+
+    //public int characterDirection; // Where the character look at
+
+        // Game Objects
+    //public GameObject tree;
+    public GameObject character;
+
+    public bool plantTreeCommanded = false; // Var for tree planting
+    private Vector2 firstPlayerPos;
+
+    //Kostyli
+    private int step = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        commandsPanel = CommandPanel.instance; // List of our commands
+
+        // CharacterMove.cs
+        firstPlayerPos = character.GetComponent<Transform>().position;
+    }
+
+        // RETRY button
+    public void RetryPressed()
+    {
+        step = 0; //KOSTYL!!!
+
+            //Reset position of character
+        character.transform.position = firstPlayerPos;
+        character.GetComponent<CharacterController>().target = firstPlayerPos;
+
+        plantTreeCommanded = false; // Reset command
+
+            //Destroy all instantiated trees
+        GameObject[] palms = GameObject.FindGameObjectsWithTag("Palm");
+        for (int i = 0; i < palms.Length; i++)
+            Destroy(palms[i]);
+    }
+        // PLAY button
+    public void PlayPressed()
+    {
+            //Read commands
+        for (int i = 0; i < commandsPanel.commands.Count; i++)
+        {
+            //Debug.Log("List:" + commandsPanel.commands[i]);
+            if (commandsPanel.commands[i].name == "Go")
+            {
+                step++;
+                character.GetComponent<CharacterController>().Move();
+            }
+            if (commandsPanel.commands[i].name == "Plant")
+            {
+                if (step == 2) // KOSTYL!!!
+                    Plant();
+            }
+        }
+    }
+
+        // Plant a tree
+    public void Plant()
+    {
+        Debug.Log("Plant function");
+        plantTreeCommanded = true;
+        //if (character.GetComponent<CharacterController>().isReadyToPlant == true)
+        //Instantiate(tree);
+    }
+}
